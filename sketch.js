a4256354d3993fbe68f04dc52296058f19c5d84e
Jocelyn Alsdorf <jfrodsla@gmail.com>
2017-07-08 09:19:25 -0700

    blue stroke added

--- sketch.js
+++ sketch.js
@@ -6,8 +6,10 @@ function setup() {
 function draw(){
   if (mouseIsPressed) {
     fill(178,0,45);
+    stroke(56,219,230);
   } else {
     fill(255);
+    stroke(56,219,230);
   }
   ellipse(mouseX,mouseY,80,80);
 }
